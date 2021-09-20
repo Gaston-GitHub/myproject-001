b44f8471d29a142c3dd27d466850926adcac331b
Gaston <gaston@github.com>
2021-09-21 00:09:13 +0200

    redux actions created

---
+++ src/actions/types.js
@@ -0,0 +1,11 @@
+// Redux Actions
+// Define string constant that indicate the type of action being performed
+
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const REGISTER_FAIL = 'REGISTER_FAIL';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
+
+export const SET_MESSAGE = 'SET_MESSAGE';
+export const CLEAR_MESSAGE = 'CLEAR_MESSAGE';

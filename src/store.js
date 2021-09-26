790c4a9785edb45bc55070fecc2b2826d1749734
Gaston <gaston@github.com>
2021-09-26 22:12:01 +0200

    created redux store

---
+++ src/store.js
@@ -0,0 +1,17 @@
+// Create redux store
+// Store will brings actions and reducers together and hold application state
+// Install redux, thunk middleware and redux devtool extension
+
+import { createStore, applyMiddleware} from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+const middleware = [thunk];
+
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(...middleware))
+)
+
+export default store;

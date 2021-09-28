42a49788d69fa6dc30f470700d98b0ee2e14a12d
Gaston <gaston@github.com>
2021-09-28 18:24:34 +0200

    created home component

---
+++ src/components/home.component.jsx
@@ -0,0 +1,49 @@
+// create React components for accessing resources
+
+//this is public page that shows public content.
+//People don't need to log in to view this page
+
+import React, { Component } from 'react';
+
+import UserService from '../services/user.service';
+
+export default class Home extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            content: ''
+        }
+    }
+
+    componentDidMount() {
+        UserService.getPublicContent().then(
+            response => {
+                this.setState({
+                    content: response.data
+                });
+            },
+            error => {
+                this.setState({
+                    content:
+                        (error.response && error.response.data) ||
+                        error.message ||
+                        error.toString()
+                });
+            }
+        );
+    }
+
+    render() {
+        return (
+            <div className='container'>
+                <header className='jumbotron'>
+                    <h3>{this.state.content}</h3>
+                </header>
+            </div>
+        )
+    }
+
+
+
+}
